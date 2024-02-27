a1b6888908589d6c77a87c6f4eb159589c07d732
Vero_bel <veronika_sia@mail.ru>
2024-02-27 12:42:12 +0200

    Add Task 2

--- main.py
+++ main.py
@@ -14,3 +14,15 @@ def read_recipes(filename):
     return cook_book
 
 read_recipes(recipes.txt)
+
+def get_shop_list_by_dishes(dishes, person_count):
+        cook_book = read_recipes(recipes.txt)
+        shop_list = {}
+        for dish in dishes:
+                for ingredient in cook_book[dish]:
+                        ingredient_name = ingredient['ingredient_name']
+                        if ingredient_name not in shop_list:
+                                shop_list[ingredient_name] = {'measure': ingredient['measure'], 'quantity': int(ingredient['quantity']) * person_count}
+                        else:
+                                shop_list[ingredient_name]['quantity'] += int(ingredient['quantity']) * person_count
+        return shop_list
